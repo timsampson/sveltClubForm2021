d73cf00d3d07eef29b706c5da58dece5fde4160e
Tim Sampson <timsampson@gmail.com>
2021-06-27 10:24:07 +0800

    style login status btn

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -17,21 +17,29 @@
 >
   <div class="flex-initial">
     <div>
-      <a href="#/" class="hover:text-gray-500 mx-2 text-sm">Sign up</a>
-      <a href="#/home/" class="hover:text-gray-500 mx-2 text-sm">Home</a>
-      <a href="#/dashboard/" class="hover:text-gray-500 mx-2 text-sm"
+      <a href="#/" class="hover:text-gray-500 mx-2 text-sm text-white"
+        >Sign up</a
+      >
+      <a href="#/home/" class="hover:text-gray-500 mx-2 text-sm text-white"
+        >Home</a
+      >
+      <a href="#/dashboard/" class="hover:text-gray-500 mx-2 text-sm text-white"
         >Dashboard</a
       >
     </div>
   </div>
   <div>
     {#if !emailLoaded}
-      <p class="rounded-full py-1 px-6 bg-indigo-500 text-white text-sm	">
+      <p
+        class="rounded-full py-1 px-6 bg-blue-700 text-gray-300 text-sm border border-gray-300"
+      >
         Signin
       </p>
     {:else}
-      <p class="rounded-full py-1 px-6 bg-indigo-600 text-white text-sm	">
-        Logged in as: {userEmail.slice(0, userEmail.indexOf("@"))}
+      <p
+        class="rounded-full py-1 px-6 bg-blue-700 text-white text-sm border border-gray-300	"
+      >
+        {userEmail.slice(0, userEmail.indexOf("@"))}
       </p>
     {/if}
   </div>
