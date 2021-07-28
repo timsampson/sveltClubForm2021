6272ee79b98fb7651c820bb152060cafad7c3d50
Tim Sampson <timsampson@gmail.com>
2021-07-28 08:57:11 +0800

    button component

---
+++ src/components/ui/Button.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let submitted;
+</script>
+
+<button
+  on:click
+  class="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg inline-flex items-center mt-4 mr-2"
+  class:bg-green-500={submitted}
+  class:hover:bg-green-700={submitted}
+  class:bg-blue-500={!submitted}
+  class:hover:bg-blue-700={!submitted}
+>
+  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+    <path
+      fill-rule="evenodd"
+      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+      clip-rule="evenodd"
+    />
+  </svg>
+  <span class="ml-2"><slot /></span>
+</button>
